aae8d73b49dc27730acad217962251a66701460b
Georgi Chochev <georgichochev94@gmail.com>
2021-10-26 01:50:27 +0300

    New Logo

--- src/renderer/layout/HeaderComponent.tsx
+++ src/renderer/layout/HeaderComponent.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import sgLogo from '../../../assets/icons/logo.png';
+import sgLogo from '../../../assets/icons/stuffnyata@20.png';
 import { Header, Logo } from './Layout.styles';
+
+/*
+Custom Logo svg can be found in the assets folder stuffnyata.svg
+made at https://boxy-svg.com/app/disk:5VwTTsTI7S
+*/
 
 const HeaderComponent: React.FC = () => {
   return (
